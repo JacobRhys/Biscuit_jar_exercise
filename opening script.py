e369d0377164b364a01115c43386ae4e8c3e1136
JacobRhys <JacobRhys04@gmail.com>
2023-08-29 22:10:36 +0100

    __init__

--- opening script.py
+++ opening script.py
@@ -1,1 +1,15 @@
-import pyserial
+import serial
+
+# Open the serial port (change the port number to the port of your arduino)
+port = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+
+#code to open and close the jar
+command = input("open or close the jar?: ")
+
+if command=="open":
+    port.write(bytes("open", 'utf-8'))
+elif command=="close":
+    port.write(bytes("close", 'utf-8'))
+
+
+
